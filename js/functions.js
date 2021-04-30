0faadde2d03be0e6e500477e392a67b8389e85a8
elduderinow <yarrut.franken@gmail.com>
2021-04-30 10:23:51 +0200

    deleted unused js, added new comments

--- js/functions.js
+++ js/functions.js
@@ -1,15 +1,16 @@
 
-
+//Creating the cards and randomize it on every reload.
 let mainDeck = ["card-a", "card-a", "card-b", "card-b", "card-c", "card-c", "card-d", "card-d", "card-e", "card-e", "card-f", "card-f", "card-g", "card-g", "card-h", "card-h", "card-i", "card-i", "card-j", "card-j", "card-k", "card-k", "card-l", "card-l"];
 mainDeck = mainDeck.sort(() => Math.random() - 0.5);
 let compArr = [];
 let compData = [];
 
-
+//Eventlistener for every reload
 document.getElementById("play").addEventListener("click",function (){
     location.reload();
 
 });
+
 
 startgame();
 
@@ -47,6 +48,7 @@ Array.from(document.querySelectorAll("div.cards-inner")).forEach(function ($btn,
     });
 });
 
+//When clicked two cards, they get send to a compare array. which triggers a if else statement.
 function compare($btn) {
 
     if (compArr[0] === compArr[1]) {
@@ -61,14 +63,16 @@ function compare($btn) {
     }
 }
 
+//If they are the same cards, then add a class 'correct'
 function ifequal($btn) {
     Array.from(document.getElementsByClassName(compArr[0])).forEach((elem) => {
         elem.classList.add("correct");
     });
     compArr = [];
 }
+}
 
-//turn all cards around
+//if it's not equal, turn the two last cards around.
 function turnAround($btn) {
     Array.from(document.getElementsByClassName(compArr[0])).forEach((elem) => {
         elem.classList.add("card-hidden");
